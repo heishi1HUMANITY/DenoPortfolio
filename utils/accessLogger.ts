5e495c6bda4fba92ee637730bd370ed1df37e299
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2022-08-06 17:17:56 +0900

    linter対策

--- utils/accessLogger.ts
+++ utils/accessLogger.ts
@@ -2,7 +2,7 @@ export const logger = async(from: Deno.NetAddr, url: string) => {
   try {
     const domain = await Deno.resolveDns(`${from.hostname.split('.').reverse().join('.')}.in-addr.arpa`, 'PTR');
     console.log(`access from ${from.hostname} (${domain[0]}) to ${url}`);
-  } catch (e) {
+  } catch (_) {
     console.log(`access from ${from.hostname} to ${url.toString()}`)
   }
 }

3a78208dd9356994cb2536be095e49e51daee283
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2022-08-06 17:15:52 +0900

    アクセス元のアドレスとアクセス先を表示するloggerを追加

---
+++ utils/accessLogger.ts
@@ -0,0 +1,8 @@
+export const logger = async(from: Deno.NetAddr, url: string) => {
+  try {
+    const domain = await Deno.resolveDns(`${from.hostname.split('.').reverse().join('.')}.in-addr.arpa`, 'PTR');
+    console.log(`access from ${from.hostname} (${domain[0]}) to ${url}`);
+  } catch (e) {
+    console.log(`access from ${from.hostname} to ${url.toString()}`)
+  }
+}

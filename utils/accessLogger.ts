30359738b125dcdfe68595244c9060b1c0c59e4c
heishi1HUMANITY <i.wanna.make.this.main@gmail.com>
2022-08-06 17:56:17 +0900

    [format]

--- utils/accessLogger.ts
+++ utils/accessLogger.ts
@@ -10,18 +10,23 @@ export const logger = async (from: Deno.NetAddr, url: string) => {
   }
 };
 
-const isV4 = (addr: Deno.NetAddr): boolean => addr.hostname.includes('.');
+const isV4 = (addr: Deno.NetAddr): boolean => addr.hostname.includes(".");
 
-const createIpv4PtrRequest = (addr: Deno.NetAddr): string => `${addr.hostname.split('.').reverse().join('.')}.in-addr.arpa`;
+const createIpv4PtrRequest = (addr: Deno.NetAddr): string =>
+  `${addr.hostname.split(".").reverse().join(".")}.in-addr.arpa`;
 
 const createIpv6PtrRequest = (addr: Deno.NetAddr): string => {
-  let segments = addr.hostname.split(':');
-  if (segments.includes('')) {
+  let segments = addr.hostname.split(":");
+  if (segments.includes("")) {
     segments = [
-      ...segments.slice(0, segments.indexOf('')),
-      ...Array(8 - (segments.length - 1)).fill('0'),
-      ...segments.slice(segments.indexOf('') + 1, segments.length),
+      ...segments.slice(0, segments.indexOf("")),
+      ...Array(8 - (segments.length - 1)).fill("0"),
+      ...segments.slice(segments.indexOf("") + 1, segments.length),
     ];
   }
-  return `${segments.reverse().map(segment => Array.from(segment.padStart(4, '0')).reverse().join('.')).join('.')}.ip6.arpa`
-}
+  return `${
+    segments.reverse().map((segment) =>
+      Array.from(segment.padStart(4, "0")).reverse().join(".")
+    ).join(".")
+  }.ip6.arpa`;
+};
